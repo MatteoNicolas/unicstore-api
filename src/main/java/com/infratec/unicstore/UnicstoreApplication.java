c8be31cec0a23518f84e01e9a6b8d8c06cb6b11d
MatteoNicolas <matteonicolasjob@gmail.com>
2021-09-11 18:24:41 -0300

    commit inicial

---
+++ src/main/java/com/infratec/unicstore/UnicstoreApplication.java
@@ -0,0 +1,13 @@
+package com.infratec.unicstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnicstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnicstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infratec/unicstore/UnicstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infratec.unicstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnicstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
